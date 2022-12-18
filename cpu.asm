c975777ed110264551fde3724b4732ef0504c896
Joonas Lehto <joonas.eemil.lehto@gmail.com>
2022-12-18 16:49:33 +0200

    Remove unused legacy PIC code and automatically disable interrupts before interrupt handlers

--- cpu.asm
+++ cpu.asm
@@ -83,6 +83,8 @@ ret
 .align 32
 .global interrupt_entry
 interrupt_entry:
+cli # Disable interrupts
+
 # Save all the registers
 push r15
 push r14
@@ -131,6 +133,8 @@ pop r14
 pop r15
 
 add rsp, 16 # Remove the interrupt number and padding (Added by the interrupt entry)
+
+sti # Enable interrupts
 iretq
 
 .global get_interrupt_handler
@@ -138,89 +142,11 @@ get_interrupt_handler:
 lea rax, [interrupt_entry]
 ret
 
-irq_enable:
-
-    # Move IRQ into cl.
-    mov     rcx,    rdi
-
-    # Determine which PIC to update (<8 = master, else slave).
-    cmp     cl,     8
-    jae     irq_enable_slave
-
-    irq_enable_master:
-
-        # Compute the mask ~(1 << IRQ).
-        mov     edx,    1
-        shl     edx,    cl
-        not     edx
-
-        # Read the current mask.
-        in      al,     0x21
-
-        # Clear the IRQ bit and update the mask.
-        and     al,     dl
-        out     0x21,   al
-
-        ret
-
-    irq_enable_slave:
-
-        # Recursively enable master IRQ2, or else slave IRQs will not work.
-        mov     rdi,    2
-        call    irq_enable
-
-        # Subtract 8 from the IRQ.
-        sub     cl,     8
-
-        # Compute the mask ~(1 << IRQ).
-        mov     edx,    1
-        shl     edx,    cl
-        not     edx
-
-        # Read the current mask.
-        in      al,     0xa1
-
-        # Clear the IRQ bit and update the mask.
-        and     al,     dl
-        out     0xa1,   al
-
-        ret
-
 # idt = 0x9000
 # idtr = 0x8000
 
 .global test_interrupt
 test_interrupt:
-# Initialize the master PIC.
-mov     al,     0x11        # ICW1: 0x11 = init with 4 ICW's
-out     0x20,   al
-mov     al,     0x20        # ICW2: 0x20 = interrupt offset 32
-out     0x21,   al
-mov     al,     0x04        # ICW3: 0x04 = IRQ2 has a slave
-out     0x21,   al
-mov     al,     0x01        # ICW4: 0x01 = x86 mode
-out     0x21,   al
-
-# Initialize the slave PIC.
-mov     al,     0x11        # ICW1: 0x11 = init with 4 ICW's
-out     0xa0,   al
-mov     al,     0x28        # ICW2: 0x28 = interrupt offset 40
-out     0xa1,   al
-mov     al,     0x02        # ICW3: 0x02 = attached to master IRQ2.
-out     0xa1,   al
-mov     al,     0x01        # ICW4: 0x01 = x86 mode
-out     0xa1,   al
-
-# Disable all IRQs. The kernel will re-enable the ones it wants to
-# handle later.
-mov     al,     0xff
-out     0x21,   al
-out     0xa1,   al
-
 # Register an empty interrupt descriptor table
 lidt [0x8000]
-
-mov rdi, 1
-call irq_enable
-
 ret

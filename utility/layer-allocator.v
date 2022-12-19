0e43df97c6095618f8b9f57ad86557af91dffe80
Joonas Lehto <joonas.eemil.lehto@gmail.com>
2022-12-19 19:10:17 +0200

    Continue the layer allocator

--- utility/layer-allocator.v
+++ utility/layer-allocator.v
@@ -3,10 +3,10 @@ LayerAvailableElement {
 	previous: LayerAvailableElement
 }
 
-pack Layer {
+Layer {
 	depth: u32
-	upper: Layer*
-	lower: Layer*
+	upper: Layer
+	lower: Layer
 
 	states: link
 	size: u64
@@ -25,11 +25,11 @@ pack Layer {
 			return slab
 		}
 
-		slab: link = (physical_address as u64) & (!(size - 1))
+		slab: link = (physical_address as u64) & (-size)
 
 		if is_split(slab) {
 			# Since the slab on the current layer is already split, continue lower
-			return lower[].split(physical_address, to)
+			return lower.split(physical_address, to)
 		}
 
 		index = (slab as u64) / size
@@ -40,42 +40,14 @@ pack Layer {
 		set_unavailable(index)
 
 		# Compute the addresses of the two lower layer slabs
-		slab = physical_address & (!(lower[].size - 1))
-		buddy = ((slab as u64) ¤ lower[].size) as link
+		slab = physical_address & (-lower.size)
+		buddy = ((slab as u64) ¤ lower.size) as link
 
 		# Set the buddy slab available on the lower layer
-		lower[].add(buddy)
+		lower.add(buddy)
 
 		# Since we have not reached the target depth, continue lower
-		return lower[].split(physical_address, to)
-	}
-
-	# Summary:
-	# Splits slabs towards the specified physical address down to the specified depth.
-	# Allocates the lowest slab containing the specified physical address.
-	split_without_adding_available_slabs(physical_address: link, to: u32): link {
-		# Stop when the target depth is reached
-		if depth == to {
-			index = (slab as u64) / size
-			set_unavailable(index)
-			return slab
-		}
-
-		slab: link = (physical_address as u64) & (!(size - 1))
-
-		if is_split(slab) {
-			# Since the slab on the current layer is already split, continue lower
-			return lower[].split_without_adding_available_slabs(physical_address, to)
-		}
-
-		index = (slab as u64) / size
-		require(is_available(index), 'Can not split an unavailable slab')
-
-		# Since we are splitting the specified slab, it must be set unavailable
-		set_unavailable(index)
-
-		# Since we have not reached the target depth, continue lower
-		return lower[].split_without_adding_available_slabs(physical_address, to)
+		return lower.split(physical_address, to)
 	}
 
 	# Summary:
@@ -112,20 +84,7 @@ pack Layer {
 		left = index * 2
 		right = left + 1
 
-		return not lower[].is_available(left) or not lower[].is_available(right)
-	}
-
-	# Summary:
-	# Adds all available slabs to available entries
-	add_available_slabs(slab: link) {
-		if is_available((slab as u64) / size) {
-			add(slab)
-		}
-
-		if not is_split(slab) return
-
-		lower[].add_available_slabs(slab)
-		lower[].add_available_slabs(slab + lower[].size)
+		return not lower.is_available(left) or not lower.is_available(right)
 	}
 
 	# Summary: Returns whether the specified entry is available
@@ -133,7 +92,7 @@ pack Layer {
 		byte = index / 8
 		bit = index - byte * 8
 
-		return (states[byte] |> bit) & 1
+		return ((states[byte] |> bit) & 1) == 0
 	}
 
 	# Summary: Sets the specified entry available
@@ -141,7 +100,7 @@ pack Layer {
 		byte = index / 8
 		bit = index - byte * 8
 
-		states[byte] |= (1 <| bit)
+		states[byte] &= (!(1 <| bit))
 	}
 
 	# Summary: Sets the specified entry unavailable
@@ -149,7 +108,7 @@ pack Layer {
 		byte = index / 8
 		bit = index - byte * 8
 
-		states[byte] &= (!(1 <| bit))
+		states[byte] |= (1 <| bit)
 	}
 
 	# Summary:
@@ -160,12 +119,22 @@ pack Layer {
 
 	# Summary: Adds the specified entry into the available entries
 	add(physical_address: link) {
+		# Map the slab so that we can write into it
+		kernel.mapper.map_page(physical_address, physical_address)
+
 		element = physical_address as LayerAvailableElement
 		element.next = none as LayerAvailableElement
 		element.previous = last
 
 		# Connect the currently last element to this new element
-		last.next = element
+		if last !== none {
+			last.next = element
+		}
+
+		# Update the next entry if there is none
+		if next === none {
+			next = element
+		}
 
 		# Update the last entry
 		last = element
@@ -247,37 +216,41 @@ LayerAllocator {
 		instance = LayerAllocator(memory_information) using address
 	}
 
-	constant MAX_MEMORY = 512000000000 # 512 GB
 	constant LAYER_COUNT = 8
 
-	constant L0_SIZE = 0x800000
-	constant L1_SIZE = 0x400000
-	constant L2_SIZE = 0x200000
-	constant L3_SIZE = 0x100000
-	constant L4_SIZE = 0x80000
-	constant L5_SIZE = 0x40000
-	constant L6_SIZE = 0x20000
-	constant L7_SIZE = 0x10000
+	constant L0_SIZE = 0x80000
+	constant L1_SIZE = 0x40000
+	constant L2_SIZE = 0x20000
+	constant L3_SIZE = 0x10000
+	constant L4_SIZE = 0x8000
+	constant L5_SIZE = 0x4000
+	constant L6_SIZE = 0x2000
+	constant L7_SIZE = 0x1000
 
-	constant L0_COUNT = (MAX_MEMORY / L0_SIZE)
-	constant L1_COUNT = (MAX_MEMORY / L1_SIZE)
-	constant L2_COUNT = (MAX_MEMORY / L2_SIZE)
-	constant L3_COUNT = (MAX_MEMORY / L3_SIZE)
-	constant L4_COUNT = (MAX_MEMORY / L4_SIZE)
-	constant L5_COUNT = (MAX_MEMORY / L5_SIZE)
-	constant L6_COUNT = (MAX_MEMORY / L6_SIZE)
-	constant L7_COUNT = (MAX_MEMORY / L7_SIZE)
+	constant L0_COUNT = (kernel.mapper.MAX_MEMORY / L0_SIZE)
+	constant L1_COUNT = (kernel.mapper.MAX_MEMORY / L1_SIZE)
+	constant L2_COUNT = (kernel.mapper.MAX_MEMORY / L2_SIZE)
+	constant L3_COUNT = (kernel.mapper.MAX_MEMORY / L3_SIZE)
+	constant L4_COUNT = (kernel.mapper.MAX_MEMORY / L4_SIZE)
+	constant L5_COUNT = (kernel.mapper.MAX_MEMORY / L5_SIZE)
+	constant L6_COUNT = (kernel.mapper.MAX_MEMORY / L6_SIZE)
+	constant L7_COUNT = (kernel.mapper.MAX_MEMORY / L7_SIZE)
 
-	constant LAYER_STATE_MEMORY_SIZE = (L0_COUNT + L1_COUNT + L2_COUNT + L3_COUNT + L4_COUNT + L5_COUNT + L6_COUNT + L7_COUNT) / 4
+	constant LAYER_STATE_MEMORY_SIZE = (L0_COUNT + L1_COUNT + L2_COUNT + L3_COUNT + L4_COUNT + L5_COUNT + L6_COUNT + L7_COUNT) / 8
 
 	layers: Layer[LAYER_COUNT]
 
 	init(memory_information: kernel.SystemMemoryInformation) {
 		reserved = memory_information.reserved
 
-		states = this as link + capacityof(LayerAllocator)
-		upper = layers as Layer*
-		lower = layers as Layer* + capacityof(Layer)
+		# Setup the layers
+		loop (i = 0, i < LAYER_COUNT, i++) {
+			layers[i] = this as link + capacityof(LayerAllocator) + i * capacityof(Layer)
+		}
+
+		states = this as link + capacityof(LayerAllocator) + LAYER_COUNT * capacityof(Layer)
+		upper = (this as link + capacityof(LayerAllocator)) as Layer
+		lower = (this as link + capacityof(LayerAllocator) + capacityof(Layer)) as Layer
 		count = L0_COUNT
 		size = L0_SIZE
 
@@ -288,7 +261,7 @@ LayerAllocator {
 			layers[i].states = states
 			layers[i].size = size
 
-			states += count / 8
+			states += count / 8 # TODO: Can the result be uneven and break things?
 			count *= 2
 			size /= 2
 			upper += capacityof(Layer)
@@ -296,32 +269,93 @@ LayerAllocator {
 		}
 
 		# Fix the first and last layer
-		layers[0].upper = none as Layer*
-		layers[LAYER_COUNT - 1].lower = none as Layer*
-
-		return
+		layers[0].upper = none as Layer
+		layers[LAYER_COUNT - 1].lower = none as Layer
 
 		# Set all the reserved segments unavailable
 		loop (i = 0, i < reserved.size, i++) {
-			reserve_without_adding_available_slabs(reserved[i])
+			reserve_region(reserved[i])
 		}
 
-		# TODO: Generate the correct number of L0 entries here
-		# TODO: Use lazy generation?
-		loop (i = 0, i < 10, i++) {
-			address = (i * L0_SIZE) as link
-			layers[0].add(address)
-			layers[0].add_available_slabs(address)
+		# Compute the number of L0 slabs needed for the whole physical memory
+		slabs = memory_information.physical_memory_size / L0_SIZE
+		if slabs * L0_SIZE < memory_information.physical_memory_size { slabs++ }
+
+		loop (i = 0, i < slabs, i++) {
+			if layers[0].is_available(i) {
+				layers[0].add((i * L0_SIZE) as link)
+			}
+		}
+
+		return
+		print(32)
+
+		iterator = layers[0].next
+
+		loop (iterator !== none) {
+			debug.write('Available ') debug.write_address(iterator as link) debug.write_line()
+
+			iterator = iterator.next
+		}
+	}
+
+	print(depth: u8, slab: u64) {
+		layer = layers[depth]
+		address = slab * layer.size
+
+		if layer.is_available(slab) {
+			loop (i = 0, i < (layer.size / L7_SIZE), i++) {
+				loop (j = 0, j < LAYER_COUNT - depth - 1, j++) {
+					debug.put(` `)
+				}
+
+				debug.write(' --- ')
+				debug.write_address(address + i * L7_SIZE)
+				debug.write_line()
+			}
+
+			return
+		}
+
+		if layer.is_split((slab * layer.size) as link) {
+			print(depth + 1, slab * 2)
+			print(depth + 1, slab * 2 + 1)
+			return
+		}
+
+		loop (i = 0, i < (layer.size / L7_SIZE), i++) {
+			loop (j = 0, j < LAYER_COUNT - depth - 1, j++) {
+				debug.put(` `)
+			}
+
+			debug.write('# --- ')
+			debug.write_address(address + i * L7_SIZE)
+			debug.write_line()
+		}
+	}
+
+	print(n: u64) {
+		loop (i = 0, i < n, i++) {
+			debug.put(`7`)
+			debug.put(`6`)
+			debug.put(`5`)
+			debug.put(`4`)
+			debug.put(`3`)
+			debug.put(`2`)
+			debug.put(`1`)
+			debug.put(`0`)
+			debug.write(' --- ')
+			debug.write_address(i * L0_SIZE)
+			debug.write_line()
+
+			print(0, i)
 		}
 	}
 
 	# Summary:
 	# Returns the most suitable layer (index) for the specified amount of bytes.
 	layer_index(bytes: u64) {
-		if bytes > L0_SIZE {
-			panic('Too large allocation (unsupported)')
-		}
-
+		if bytes > L0_SIZE panic('No layer can store the specified amount of bytes')
 		if bytes > L1_SIZE return 0
 		if bytes > L2_SIZE return 1
 		if bytes > L3_SIZE return 2
@@ -329,30 +363,37 @@ LayerAllocator {
 		if bytes > L5_SIZE return 4
 		if bytes > L6_SIZE return 5
 		if bytes > L7_SIZE return 6
-
 		return 7
 	}
 
-	private reserve_without_adding_available_slabs(segment: Segment) {
-		bytes = (segment.end - segment.start) as u64
-		layer = layers[layer_index(bytes)]
-		start = segment.start & (!(layer.size - 1))
-		end = (segment.start + bytes) & (!(layer.size - 1))
-
-		layers[0].split_without_adding_available_slabs(start, layer.depth)
-
-		# If the reserved memory area uses two slabs, reserve both
-		if end != start {
-			layers[0].split_without_adding_available_slabs(end, layer.depth)
+	# Summary: Reserves the specified region by using L0 slabs
+	private reserve_region(segment: Segment) {
+		if segment.size > L0_SIZE {
+			reserve_region(Segment.new(segment.type, segment.start, segment.start + segment.size / 2))
+			reserve_region(Segment.new(segment.type, segment.start + segment.size / 2, segment.end))
+			return
 		}
+
+		# Find the start and end address of the specified segment so that they are aligned with the layer
+		start = segment.start & (-L0_SIZE)
+		end = (segment.start + segment.size) & (-L0_SIZE)
+
+		debug.write('Reserving region ')
+		debug.write_address(start) debug.put(`-`) debug.write_address(end + L0_SIZE)
+		debug.write(' at layer ') debug.write(0)
+		debug.write(' (') debug.write(L0_SIZE) debug.write_line(' bytes)')
+
+		# Set both the start and end slab unavailable
+		layers[0].set_unavailable((start / L0_SIZE) as u64)
+		layers[0].set_unavailable((end / L0_SIZE) as u64)
 	}
 
 	# Summary:
 	# Reserves the specified memory area.
 	reserve(physical_address: link, bytes: u64) {
 		layer = layers[layer_index(bytes)]
-		start = physical_address & (!(layer.size - 1))
-		end = (physical_address + bytes) & (!(layer.size - 1))
+		start = physical_address & (-layer.size)
+		end = (physical_address + bytes) & (-layer.size)
 
 		layers[0].split(start, layer.depth)
 
@@ -387,27 +428,44 @@ LayerAllocator {
 
 		# If this point is reached, there is no continuous slab available that could hold the specified amount of memory.
 		# However, we can still try using multiple slabs to hold specified amount of memory since we are using virtual addresses.
-		panic('TODO: Fragmented allocation')
+		# In addition, the size of the allocation might be problematic to determine upon deallocation.
+		panic('Out of memory')
+	}
+
+	# Summary:
+	# Allocates the specified amount of bytes and maps it to the specified virtual address.
+	# Returns the physical address of the allocated memory.
+	allocate(bytes: u64, virtual_address: link): link {
+		require((virtual_address & (PAGE_SIZE - 1)) == 0, 'Virtual address was not aligned correctly')
+
+		physical_address = allocate(bytes)
+		mapper.map_region(virtual_address, physical_address, bytes)
+
+		return physical_address
 	}
 
 	# Summary: Deallocates the specified memory.
 	deallocate(address: link) {
-		require(((address as u64) & (L7_SIZE - 1)) == 0, 'Physical address was not aligned correctly')
+		require((address & (L7_SIZE - 1)) == 0, 'Physical address was not aligned correctly')
 
-		if layers[7].owns(address) return layers[7].deallocate(virtual_address)
-		if layers[6].owns(address) return layers[6].deallocate(virtual_address)
-		if layers[5].owns(address) return layers[5].deallocate(virtual_address)
-		if layers[4].owns(address) return layers[4].deallocate(virtual_address)
-		if layers[3].owns(address) return layers[3].deallocate(virtual_address)
-		if layers[2].owns(address) return layers[2].deallocate(virtual_address)
-		if layers[1].owns(address) return layers[1].deallocate(virtual_address)
-		if layers[0].owns(address) return layers[0].deallocate(virtual_address)
+		if layers[7].owns(address) return layers[7].deallocate(address)
+		if layers[6].owns(address) return layers[6].deallocate(address)
+		if layers[5].owns(address) return layers[5].deallocate(address)
+		if layers[4].owns(address) return layers[4].deallocate(address)
+		if layers[3].owns(address) return layers[3].deallocate(address)
+		if layers[2].owns(address) return layers[2].deallocate(address)
+		if layers[1].owns(address) return layers[1].deallocate(address)
+		if layers[0].owns(address) return layers[0].deallocate(address)
 
 		panic('Can not deallocate memory that has not been allocated')
 	}
 
 	# Summary: Deallocates the specified memory and unmaps it from the specified virtual address.
 	deallocate(address: link, virtual_address: link) {
+		physical_address = mapper.to_physical_address(virtual_address)
+		if physical_address == INVALID_PHYSICAL_ADDRESS panic('Virtual addresses was not mapped upon deallocation')
 
+		size = deallocate(physical_address)
+		mapper.unmap_region(virtual_address, size)
 	}
 }
